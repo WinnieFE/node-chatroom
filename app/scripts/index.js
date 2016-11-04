5a6abfcaefa55338bea291bb2b1a750c05379fb0
彼岸冰 <706670363@qq.com>
2016-11-04 16:49:44 +0800

    移除多余文件，修改配置文件

--- app/scripts/index.js
+++
@@ -1,9 +0,0 @@
-let userName,socket,tbxUsername,tbxMsg,divChat;
-
-window.onload = function(){
-	divChat = document.getElementById('chat-main');
-	tbxUsername = document.getElementById('username');
-	tbxMsg = document.getElementById('chat-message');
-	tbxUsername.focus();
-	tbxUsername.select();
-}
